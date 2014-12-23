ac6dadb6068357dc14fb63d8900f62c459027a0f
takumin <takumiiinn@gmail.com>
2014-12-23 09:27:23 +0900

    fix

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,4 +3,11 @@
 # This class is called from freebsd_kld for service config.
 #
 class freebsd_kld::config {
+  define boot_load ($name) {
+    sysrc { "${name}_load":
+      value  => "YES",
+      rcfile => '/boot/loader.conf',
+    }
+  }
+  create_resources(boot_load, $name)
 }
